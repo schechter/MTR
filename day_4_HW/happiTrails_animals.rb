9b8dd26fd2922939dc67b844647f19bfee59177a
Mike Schechter <schechtermike@gmail.com>
2013-05-23 18:43:45 -0400

    updates main and animals

--- day_4_HW/happiTrails_animals.rb
+++ day_4_HW/happiTrails_animals.rb
@@ -1,11 +1,15 @@
 class Animal
-    attr_accessor :name, :breed, :age, :gender, :favorite_toy, :owner
+  attr_accessor :name, :breed, :age, :gender, :favorite_toy, :owner
 
-    def initialize(name, breed, age, gender, favorite_toy, owner = nil)
-        @name = name
-        @breed = breed
-        @age = age
-        @gender = gender
-        @favorite_toy = favorite_toy
-        @owner = owner
-    end
+  def initialize(name, breed, age, gender, favorite_toy, owner = nil)
+    @name = name
+    @breed = breed
+    @age = age
+    @gender = gender
+    @favorite_toy = favorite_toy
+    @owner = owner
+  end
+  def to_s
+    "#{@name}, the #{breed}, #{gender} is #{age} year's old and loves to play with #{favorite_toy}'s."
+  end
+end
---
+++ day_4_HW/happiTrails_main.rb
@@ -0,0 +1,29 @@
+require_relative 'happiTrails_shelter'
+require_relative 'happiTrails_client'
+require_relative 'happiTrails_animals'
+
+shelter_1 = Shelter.new([],[])
+
+client_1 = Client.new('Mike', '36', 'Male', false, [])
+client_2 = Client.new('Lee', '26', 'Male', false, [])
+
+pet_1 = Animal.new('Fluffy', 'Boxer', '3', 'f', 'bone')
+pet_2 = Animal.new('Spot', 'Golden', '7', 'm', 'shoes')
+pet_3 = Animal.new('Steve', 'house cat', '2', 'm', 'tin foil balls')
+pet_4 = Animal.new('Fred', 'Rock', '3.2 billion', 'm', 'windows')
+
+shelter_1.add_animal(pet_1)
+shelter_1.add_animal(pet_2)
+shelter_1.add_animal(pet_3)
+shelter_1.add_animal(pet_4)
+
+client_1.adopt_animal(shelter_1, pet_1)
+client_2.adopt_animal(shelter_1, pet_3)
+shelter.animals
+client_1.adopt_animal(shelter_1, pet_4)
+client_2.adopt_animal(shelter_1,pet_2)
+shelter.animals
+client_1.give_animal(shelter_1, pet_4)
+
+shelter.animals
+shelter.clients
