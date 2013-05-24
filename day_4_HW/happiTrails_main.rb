75c53b8a2e1189bf1fa0a8645afeafc9572b864a
Mike Schechter <schechtermike@gmail.com>
2013-05-24 12:32:42 -0400

    colorize and refactor

--- day_4_HW/happiTrails_main.rb
+++ day_4_HW/happiTrails_main.rb
@@ -2,10 +2,21 @@ require_relative 'happiTrails_shelter'
 require_relative 'happiTrails_client'
 require_relative 'happiTrails_animals'
 require_relative 'happiTrails_customer_interface'
+require_relative 'happiTrails_generators'
 require 'rainbow'
 require 'pry'
 
-shelter_1 = Shelter.new([],[])
+shelter_1 = Shelter.new
+puts `clear`
+shelters_customers= client_builder #builds the customer base
+shelters_customers.each { |c| shelter_1.add_client(c)}  #adds clients to shelter
+puts "How any animals are already in the shelter?".color(:red)
+shelters_animals = []
+num_pets = gets.chomp.to_i
+num_pets.times {|x| shelters_animals.push(pet_generator(x+1))} #generates list of animals already in shelter
+shelters_animals.each {|a| shelter_1.add_animal(a)} # add animals to shelter
+customer_interface(shelter_1)  #runs shelters customer interface
+
 =begin
 client_1 = Client.new('Mike', '36', 'Male', false, [])
 client_2 = Client.new('Lee', '26', 'Male', false, [])
@@ -34,71 +45,3 @@ client_1.give_animal(shelter_1, pet_4)
 shelter_1.list_animals
 shelter_1.list_clients
 =end
-def client_builder  #ths will return an array of client items
-  puts "How many clients does the Happi Trails have?"
-  number_of_clients=gets.chomp.to_i
-  clients=[]
-  number_of_clients.times {|x| clients.push(client_generator(x+1))}
-  clients
-end
-
-
-def client_generator(x) #will Clients
-  puts "What is client ##{x}'s name?"
-  name=gets.chomp
-  puts "What is client ##{x}'s age?"
-  age = gets.chomp
-  puts "What is client ##{x}'s gender?"
-  gender = gets.chomp
-  gender = gender_validator(gender)
-  puts "Does client ##{x} have any kids? (y) or (n)"
-  kids = gets.chomp
-  kids = kids_validator?(kids)
-  puts "How many pets does client ##{x} have?"
-  num_pets = gets.chomp.to_i
-  pets = []
-  num_pets.times {|x| pets.push(pet_generator(x+1))}
-  Client.new(name, age, gender, kids, pets)
-end
-
-def pet_generator(pet_number)
-    puts "What is pet #{pet_number}'s name?"
-    name = gets.chomp
-    puts "What is pet #{pet_number}'s breed?"
-    breed = gets.chomp
-    puts "What is pet #{pet_number}'s age?"
-    age = gets.chomp
-    puts "What is pet #{pet_number}'s gender?"
-    gender = gets.chomp
-    gender_validator(gender)
-    puts "What is pet #{pet_number}'s favorite toy?"
-    favorite_toy = gets.chomp
-    Animal.new(name, breed, age, gender, favorite_toy)
-end
-
-def gender_validator(gender)
-  while !(gender =='m' || gender =='f')
-    gender.downcase!
-    puts "Please enter m of f"
-    gender = gets.chomp
-  end
-  gender
-end
-
-def kids_validator?(kids)
-	while !(kids =='y' || kids =='n')
-    kids.downcase!
-    puts "Please enter y or n"
-    kids = gets.chomp
-  end
-  kids
-end
-
-shelters_customers= client_builder #builds the customer base
-shelters_customers.each { |c| shelter_1.add_client(c)}  #adds clients to shelter
-puts "How any animals are already in the shelter?"
-shelters_animals = []
-num_pets = gets.chomp.to_i
-num_pets.times {|x| shelters_animals.push(pet_generator(x+1))} #generates list of animals already in shelter
-shelters_animals.each {|a| shelter_1.add_animal(a)} # add animals to shelter
-customer_interface(shelter_1)  #runs shelters customer interface
--- day_4_HW/happiTrails_shelter.rb
+++ day_4_HW/happiTrails_shelter.rb
@@ -2,17 +2,17 @@ class Shelter
 
   attr_accessor :clients, :animals
 
+  def initialize(clients = [], animals = [])
+    @clients = clients
+    @animals = animals
+  end
+
   def add_animal(animal)
     @animals.push(animal)
   end
 
   def drop_animal(animal)
     @animals.delete(animal)
-  end
-
-  def initialize(clients = [], animals = [])
-    @clients = clients
-    @animals = animals
   end
 
   def add_client(client)
@@ -26,7 +26,7 @@ class Shelter
   def list_animals_options
         i = 1
         @animals.each do |animal|
-            puts "(#{i} #{animal.name})"
+            puts "(#{i} #{animal.name})".color(:cyan)
             i += 1
         end
     end
@@ -34,7 +34,7 @@ class Shelter
   def list_clients_options
       i = 1
       @clients.each do |animal|
-        puts "(#{i} #{animal.name})"
+        puts "(#{i} #{animal.name})".color(:cyan)
         i += 1
     end
   end
