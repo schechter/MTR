e99a7efa3c8e513d24b5dc1a3551f7b2723e5118
Mike Schechter <schechtermike@gmail.com>
2013-05-24 12:41:04 -0400

    fix colors

--- day_4_HW/happiTrails_generators.rb
+++ day_4_HW/happiTrails_generators.rb
@@ -1,5 +1,5 @@
 def client_builder  #ths will return an array of client items
-  puts "How many clients does the Happi Trails have?".color(:blue)
+  puts "How many clients does the Happi Trails have?".color(:white)
   number_of_clients=gets.chomp.to_i
   clients=[]
   number_of_clients.times {|x| clients.push(client_generator(x+1))}
@@ -8,17 +8,17 @@ end
 
 
 def client_generator(x) #will Clients
-  puts "What is client ##{x}'s name?".color(:blue)
+  puts "What is client ##{x}'s name?".color(:white)
   name=gets.chomp
-  puts "What is client ##{x}'s age?".color(:blue)
+  puts "What is client ##{x}'s age?".color(:white)
   age = gets.chomp
-  puts "What is client ##{x}'s gender?".color(:blue)
+  puts "What is client ##{x}'s gender?".color(:white)
   gender = gets.chomp
   gender = gender_validator(gender)
-  puts "Does client ##{x} have any kids? (y) or (n)".color(:blue)
+  puts "Does client ##{x} have any kids? (y) or (n)".color(:white)
   kids = gets.chomp
   kids = kids_validator?(kids)
-  puts "How many pets does client ##{x} have?".color(:blue)
+  puts "How many pets does client ##{x} have?".color(:white)
   num_pets = gets.chomp.to_i
   pets = []
   num_pets.times {|x| pets.push(pet_generator(x+1))}
