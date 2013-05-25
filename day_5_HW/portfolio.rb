4ebdb4b44cbf1fec5f8ad296f94ea677e2eea7aa
Lee Hampton <leejhampton@gmail.com>
2013-05-25 02:06:29 -0400

    initial commit

---
+++ day_5_HW/portfolio.rb
@@ -0,0 +1,21 @@
+require 'yahoofinance'
+
+class Portfolio
+    attr_accessor :name, :stocks, :owner
+
+    def initialize(name, owner, stocks = {})
+        @name = name
+        @owner = owner
+        @stocks = stocks
+    end
+    
+    def self.get_current_stock_price(stock_name)
+        # Class variable because behavior does not change between instances
+        YahooFinance::get_quotes(YahooFinance::StandardQuote, 'AAPL')['AAPL'].lastTrade
+    end
+
+    def buy_stock(stock_name, num_stocks)
+        total_cost = Portfolio.get_current_stock_price(stock_name) * num_stocks
+        raise(RuntimeError, "Insufficient account funds.") if @owner.balance < total_cost
+    end
+end
